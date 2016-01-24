b52c4431c5f7d519211d808f5609b716c675262c
SchoolGuy <matrixfueller@gmail.com>
2016-01-24 12:00:41 +0100

    Added Project
    No Changes

---
+++ app/src/main/java/de/noname/enno/gcfunde/fragment_geocache_details.java
@@ -0,0 +1,19 @@
+package de.noname.enno.gcfunde;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+public class fragment_geocache_details extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_geocache_details, container, false);
+
+        return rootView;
+    }
+}
